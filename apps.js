0ae6e6e3c2d8917a63fff37f1adeaf2a8779c279
unknown <ramil.mhrmf@gmail.com>
2024-04-11 03:59:49 +0400

    first commit

---
+++ apps.js
@@ -0,0 +1,43 @@
+function addRow() {
+    var table = document.getElementById("myTable").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.rows.length);
+    var cells = [];
+
+    // Hücreler oluşturuluyor
+    for (var i = 0; i < 4; i++) {
+        cells[i] = newRow.insertCell(i);
+        if (i < 3) {
+            cells[i].innerHTML = '<input type="text" placeholder="' + ["Ad", "Soyad", "Yaş"][i] + '">';
+        } else {
+            cells[i].innerHTML = '<button onclick="editRow(this)">Düzəliş Et</button>';
+        }
+    }
+}
+
+function editRow(button) {
+    var row = button.parentNode.parentNode;
+    var cells = row.getElementsByTagName('td');
+    for (var i = 0; i < cells.length - 1; i++) {
+        var cellText = cells[i].innerText;
+        cells[i].innerHTML = '<input type="text" value="' + cellText + '">';
+    }
+    var saveButton = document.createElement('button');
+    saveButton.innerHTML = 'Yadda Saxla';
+    saveButton.className = 'save-button';
+    saveButton.onclick = function() {
+        saveRow(this);
+        this.style.backgroundColor = 'red';
+    };
+    cells[3].innerHTML = '';
+    cells[3].appendChild(saveButton);
+}
+
+function saveRow(button) {
+    var row = button.parentNode.parentNode;
+    var cells = row.getElementsByTagName('td');
+    for (var i = 0; i < cells.length - 1; i++) {
+        var input = cells[i].querySelector('input');
+        cells[i].innerText = input.value;
+    }
+    cells[3].innerHTML = '<button onclick="editRow(this)">Düzəliş Et</button>';
+}
